e651ccb67ac1996a08a3f440cb43d9a95507c33c
Henk Wiedig <henk@widig.de>
2020-12-01 18:45:51 +0100

    second commit

--- does_not_work.sh
+++ does_not_work.sh
@@ -1,2 +1,3 @@
+TRUSTSTORE=/etc/ssl/certs/java/cacerts
 mvn package
-java -Djavax.net.ssl.trustStore=/etc/ssl/certs/java/cacerts -Djavax.net.ssl.trustStorePassword=changeit -jar target/demo-0.0.1-SNAPSHOT.jar
+java -Djavax.net.ssl.trustStore=$TRUSTSTORE -Djavax.net.ssl.trustStorePassword=changeit -jar target/demo-0.0.1-SNAPSHOT.jar
--- works.sh
+++ works.sh
@@ -1,2 +1,9 @@
+TRUSTSTORE=/etc/ssl/certs/java/cacerts
 mvn package
 java -jar target/demo-0.0.1-SNAPSHOT.jar
+java -Djavax.net.ssl.trustStore=$TRUSTSTORE -Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.ssl.trustStoreType=pkcs12 -jar target/demo-0.0.1-SNAPSHOT.jar
+java -Djavax.net.ssl.trustStore=$TRUSTSTORE -Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.ssl.trustStoreType=jks -jar target/demo-0.0.1-SNAPSHOT.jar
+java -Dspring.config.location=file:application_pkcs12_keystore.yml -jar target/demo-0.0.1-SNAPSHOT.jar
+java -Dspring.config.location=file:application_pkcs12_keystore.yml -Djavax.net.ssl.trustStore=$TRUSTSTORE -Djavax.net.ssl.trustStorePassword=changeit -jar target/demo-0.0.1-SNAPSHOT.jar
+java -Dspring.config.location=file:application_pkcs12_keystore.yml -Djavax.net.ssl.trustStore=$TRUSTSTORE -Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.ssl.trustStoreType=pkcs12 -jar target/demo-0.0.1-SNAPSHOT.jar
+java -Dspring.config.location=file:application_pkcs12_keystore.yml -Djavax.net.ssl.trustStore=$TRUSTSTORE -Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.ssl.trustStoreType=jks -jar target/demo-0.0.1-SNAPSHOT.jar
